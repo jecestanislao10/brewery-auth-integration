19423044a34c5366112366440302c56a8eadd628
jecestanislao10 <jestanislao@stratpoint.com>
2020-03-31 15:23:48 +0800

    preparing for deployment

--- src/infra/authentication/BreweryAuth.js
+++ src/infra/authentication/BreweryAuth.js
@@ -2,5 +2,4 @@ const Brewery = require('brewery-auth-test/src/index');
 const config = require('config/index.js');
 const auth = new Brewery(config.auth);
 
-console.log(config);
 module.exports = auth;
--- src/infra/dataSources/Db.js
+++ src/infra/dataSources/Db.js
@@ -12,32 +12,32 @@
 //   }
 // };
 // FOR TESTING
-module.exports = {
-  name: 'test-db',
-  connector : 'sql',
-  config: {
-    host: 'localhost',
-    username: 'root',
-    password: 'root',
-    database: 'yourdatabase',
-    dialect: 'mysql',
-    isSync: 'true',
-
-  }
-};
 // module.exports = {
 //   name: 'test-db',
 //   connector : 'sql',
 //   config: {
-//     host: 'remotemysql.com',
-//     username: 'I3pzK8hI33',
-//     password: 'i2kDmahSWR',
-//     database: 'I3pzK8hI33',
+//     host: 'localhost',
+//     username: 'root',
+//     password: 'root',
+//     database: 'yourdatabase',
 //     dialect: 'mysql',
 //     isSync: 'true',
 
 //   }
 // };
+module.exports = {
+  name: 'db',
+  connector : 'sql',
+  config: {
+    host: 'remotemysql.com',
+    username: 'I3pzK8hI33',
+    password: 'i2kDmahSWR',
+    database: 'I3pzK8hI33',
+    dialect: 'mysql',
+    isSync: 'true',
+
+  }
+};
 // module.exports = {
 //   name: 'test-db',
 //   connector : 'sql',
